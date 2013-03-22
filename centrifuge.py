9dfee930e414ebc7b554562f0ea19cd90933b35b
Lars Yencken <lars@yencken.org>
2013-03-23 08:03:38 +1100

    Traverse the timeline for older tweets.
    Also properly fill pages with tweets.

--- centrifuge.py
+++ centrifuge.py
@@ -6,11 +6,13 @@
 #
 
 import os
+import sys
 import cmd
 import re
 import HTMLParser
 import textwrap
 import webbrowser
+import logging
 
 from clint.textui import colored
 #import pymongo
@@ -21,27 +23,72 @@ from twitter.cmdline import CONSUMER_KEY, CONSUMER_SECRET
 DB_NAME = 'centrifuge'
 OAUTH_FILE = '~/.twitter_oauth'
 WIDTH_TOLERANCE = 2
+LOG_FILE = None
 
 
-def twitter_connect():
-    oauth_token, oauth_secret = twitter.read_token_file(
-            os.path.expanduser(OAUTH_FILE)
-        )
+if LOG_FILE:
+    logging.basicConfig(filename=os.path.expanduser(LOG_FILE),
+            level=logging.DEBUG)
 
-    t = twitter.Twitter(auth=twitter.OAuth(
-            oauth_token,
-            oauth_secret,
-            CONSUMER_KEY,
-            CONSUMER_SECRET,
-        ))
 
-    return t
+class TwitterCursor(object):
+    """
+    Provide stateful iteration up and down the timeline, anchored around the
+    initial set of tweets pulled.
+    """
+    def __init__(self):
+        logging.info('Connecting to twitter')
+        self.tw = self.connect()
+        self.above = []
+        logging.info('Asking for 20 tweets')
+        self.below = list(reversed(self.tw.statuses.home_timeline(count=20)))
+        logging.info('Got %d' % len(self.below))
+        self.oldest_seen = sys.maxint
+        self.newest_seen = None
+
+    def connect(self):
+        oauth_token, oauth_secret = twitter.read_token_file(
+                os.path.expanduser(OAUTH_FILE)
+            )
+
+        t = twitter.Twitter(auth=twitter.OAuth(
+                oauth_token,
+                oauth_secret,
+                CONSUMER_KEY,
+                CONSUMER_SECRET,
+            ))
+
+        return t
+
+    def iterolder(self):
+        while True:
+            while self.below:
+                t = self.below.pop()
+                self.oldest_seen = min(self.oldest_seen, t['id'])
+                self.newest_seen = max(self.newest_seen, t['id'])
+                yield t
+
+            self.below = list(reversed(self.tw.statuses.home_timeline(
+                    count=20, max_id=self.oldest_seen - 1)))
+
+    def pushback_older(self, t):
+        self.below.append(t)
+
+    def iternewer(self):
+        newest = self.above[0]['id'] if self.above else self.newest_seen
+        self.above[0:0] = list(reversed(self.tw.statuses.home_timeline(
+                count=20, since_id=newest)))
+        while self.above:
+            t = self.above.pop()
+            self.oldest_seen = min(self.oldest_seen, t['id'])
+            self.newest_seen = max(self.newest_seen, t['id'])
+            yield t
 
 
 class InteractiveStream(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
-        self.tw = twitter_connect()
+        self.tw = TwitterCursor()
         self.current = None
         self.h = HTMLParser.HTMLParser()
 
@@ -51,11 +98,12 @@ class InteractiveStream(cmd.Cmd):
     def do_s(self, line):
         "Fetch recent stream."
         os.system('clear')
-        tweets = self.tw.statuses.home_timeline()
         height, width = self._get_console_size()
+        logging.info('Console is %dx%d' % (width, height))
         remaining = height - 1
+        self.current = []
 
-        for i, t in enumerate(tweets):
+        for i, t in enumerate(self.tw.iterolder()):
             user = t['user']['screen_name']
             name = t['user']['name'].strip()
             text = t['text']
@@ -77,18 +125,26 @@ class InteractiveStream(cmd.Cmd):
                 ))
             lines.append(author)
 
-            tweet_height = len(lines) + 2
+            tweet_height = len(lines) + 1
+            logging.debug('Tweet height is %d, got %d left' % (
+                    tweet_height,
+                    remaining,
+                ))
             if tweet_height > remaining:
+                # XXX push back tweet?
+                logging.debug("Couldn't fit tweet -- finishing layout")
+                self.tw.pushback_older(t)
                 break
 
             lines = [l for l in self._format_lines(i, lines)]
             print '\n'.join(lines)
             print
             remaining -= tweet_height
+            logging.debug("Included tweet, got %d left" % remaining)
+            self.current.append(t)
 
-        print '\n' * (remaining - 1)
-
-        self.current = tweets[:i + 1]
+        if remaining > 1:
+            print '\n' * (remaining - 1)
 
     def do_EOF(self, l):
         return True
@@ -97,21 +153,27 @@ class InteractiveStream(cmd.Cmd):
         "Quit."
         return True
 
-    def do_o(self, i, j=None):
+    def do_o(self, *args):
         "Open the URL in a tweet."
-        i = int(i)
-        urls = self.current[i - 1]['entities']['urls']
-        if not urls:
-            print 'No urls for tweet %d' % i
+        try:
+            args = map(int, args)
+        except ValueError:
+            print 'Just number the tweets with urls you want to open.\n'
             return
 
-        if j is None:
-            for u in urls:
-                webbrowser.open_new_tab(u['url'])
-        elif 1 <= j <= len(urls):
-            webbrowser.open_new_tab(urls[j - 1]['url'])
-        else:
-            print 'url out of range'
+        print 'Opening %d urls' % len(args)
+        for i in args:
+            if i < 1 or i > len(self.current):
+                print 'No tweet numbered %d -- skipping' % i
+                continue
+
+            urls = self.current[i - 1]['entities']['urls']
+            if not urls:
+                print 'No urls for tweet %d -- skipping' % i
+                continue
+
+            # XXX what about multi-url case?
+            webbrowser.open_new_tab(urls[0]['url'])
 
     def highlight_text(self, text):
         text = re.sub(u'(@[A-Za-z0-9_]+)', str(colored.blue('\\1')), text,
